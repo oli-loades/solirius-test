f4a7e6a8f150212ad5d0e3f2c158493adde3faf2
Oli Loades <oli.loades@gmail.com>
2025-03-02 15:09:28 +0000

    Added rate limit middleware

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require('express')
 const dotenv = require('dotenv');
 const routes = require('./api')
 const errorHandler = require('./middleware/errorHandler')
+const rateLimiter = require('./middleware/rateLimiter')
 
 dotenv.config();
 
 const app = express()
 const port = process.env.PORT || 3000
 
+//gloabl middleware
+app.use(rateLimiter)
+
+// routes
 app.use(routes)
 
+//global error handler
 app.use(errorHandler)
 
 app.listen(port, () => {
---
+++ middleware/rateLimiter.js
@@ -0,0 +1,6 @@
+const rateLimit = require('express-rate-limit');
+
+module.exports = rateLimit({
+    limit: process.env.RATE_LIMIT || 10,
+    windowMs: process.env.RATE_LIMIT_WINDOW || 60000,
+})
